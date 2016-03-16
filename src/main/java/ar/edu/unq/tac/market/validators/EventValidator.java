656fdd81c23c8be1efe58893cbd419b0cb8ff239
marianovarela <marianogvarela@gmail.com>
2016-03-15 22:46:46 -0300

    clean proyect

--- src/main/java/ar/edu/unq/tac/market/validators/EventValidator.java
+++ src/main/java/ar/edu/unq/tac/market/validators/EventValidator.java
@@ -1,18 +1,8 @@
 package ar.edu.unq.tac.market.validators;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
-
-import ar.edu.unq.tac.market.repository.EventRepository;
-import ar.edu.unq.tac.market.service.EventService;
 
 @RepositoryEventHandler
 public class EventValidator {
-	
-	@Autowired
-	private EventService eventService;
-	
-	@Autowired
-	private EventRepository eventRepository;
 	
 }
